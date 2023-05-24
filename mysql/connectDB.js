39df43661891a1581523872316ea2f5af13e058e
Jasmine Regnér <jasmine.regner@outlook.com>
2023-05-24 15:22:14 +0200

    add code to connect, view and edit database

--- mysql/connectDB.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-const { db_host, db_name, db_user, db_pass } = require('../config.json');
-
-const conn = mysql.createConnection({
-	host: db_host,
-	user: db_name,
-	database: db_user,
-	password: db_pass,
-});
---
+++ mysqldb/connectDB.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2');
+const { db_host, db_name, db_user, db_pass } = require('../config.json');
+
+const dbCtrl = (() => {
+	const con = mysql.createConnection({
+		host: db_host,
+		user: db_user,
+		database: db_name,
+		password: db_pass,
+	});
+
+	function edit(sql) {
+		con.query(sql, function(err) {
+			if (err) throw err;
+		});
+	}
+
+	async function view(sql) {
+		const [ rows ] = await con.promise().query(sql);
+		return rows;
+	}
+
+	return {
+		view,
+		edit,
+	};
+})();
+
+module.exports = { dbCtrl };
